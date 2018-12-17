82ecb36b8d7c47f3d055f65ac47c520b0810657e
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2018-12-17 09:17:21 +0900

    診断したら表示できるようにした

--- js/shindan.js
+++ js/shindan.js
@@ -1,6 +1,30 @@
+const lists = [
+    {
+        id: 1,
+        name: "userA",
+        message: "テストメッセージ1"
+    },
+    {
+        id: 2,
+        name: "userA",
+        message: "テストメッセージ2"
+    }
+];
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
+        result: ''
+    },
+    methods: {
+        shindan: function () {
+            this.result = "診断結果です";
+        },
+        tweet: function () {
+            let shareURL =  "https://twitter.com/share?url=https://retoruto-carry.github.io/shindan.html&text=%3e%3e%20あなたのねこ度がわかるボタン";
+            window.open(shareURL);
+
+        }
+    },
+
 })
